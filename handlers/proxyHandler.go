b3694e7d64775cb196a9cf5d2fb8881e53c6315e
allwsaa <bekbosynova.alima@inbox.ru>
2024-07-08 02:19:13 +0500

    proxy

--- handlers/proxyHandler.go
+++ handlers/proxyHandler.go
@@ -1,1 +1,36 @@
 package handlers
+
+import (
+	"encoding/json"
+	"http-proxy-task1/models"
+	"io"
+	"net/http"
+	"sync"
+)
+
+var reqStore sync.Map
+
+func prHandler(w http.ResponseWriter, r *http.Request) {
+	var req models.Request
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Fail", http.StatusBadRequest)
+		return
+	}
+
+	//chck url
+	resp, err := http.Get(req.URL)
+	if err != nil {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+	defer resp.Body.Close()
+
+	//body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Failed to read response", http.StatusInternalServerError)
+		return
+	}
+
+}
