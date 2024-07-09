5f2555c8d52a52544914782b4222276323c0bbb8
allwsaa <bekbosynova.alima@inbox.ru>
2024-07-09 23:51:00 +0500

    -

---
+++ handlers/healthCheck.go
@@ -0,0 +1,10 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func healthCheckHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	//get/post
+	r.Get("/health", handlers.healthCheckHandler)
+	r.Post("/proxy", handlers.prHandler)
+
+	log.Println("Starting server: 8080")
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
